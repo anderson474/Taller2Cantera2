27e385c652a90ce317dc9dea75753de94c2670dc
= <=>
2022-09-24 20:48:41 -0500

    agregue las funciones pero sin completar ni concretar nada

---
+++ src/nave/Lanzadera.java
@@ -0,0 +1,9 @@
+public class Lanzadera {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
+/**
+ * tres tipos de vehiculos: lanzadera, no tripuladas o roboticas
+ * y tripuladas
+ */
